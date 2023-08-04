f700edab53a8fae125df826e01570f605dc9b38b
Mohammed alJaser <aljaser.develop@gmail.com>
2023-08-04 14:45:28 +0200

    Completed the coding challenge

---
+++ GildedRose.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                // Legendary item "Sulfuras" never decreases in Quality and does not have to be sold.
+                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    continue;
+                }
+
+                // Decrease the Quality for normal items (not "Aged Brie", "Backstage passes", or "Conjured Mana Cake")
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Conjured Mana Cake")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        // Decrease Quality by 1 for normal items before SellIn date
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
+                }
+                else // Handle "Aged Brie", "Backstage passes", and "Conjured Mana Cake"
+                {
+                    if (Items[i].Quality < 50) // Ensure Quality does not exceed 50
+                    {
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn <= 0)
+                            {
+                                // Quality drops to 0 after the concert
+                                Items[i].Quality = 0;
+                            }
+                            else if (Items[i].SellIn <= 5)
+                            {
+                                // Quality increases by 3 when there are 5 days or less until the concert
+                                Items[i].Quality = Items[i].Quality + 3;
+                            }
+                            else if (Items[i].SellIn <= 10)
+                            {
+                                // Quality increases by 2 when there are 10 days or less until the concert
+                                Items[i].Quality = Items[i].Quality + 2;
+                            }
+                            else
+                            {
+                                // Quality increases by 1 before 10 days until the concert
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                        else if (Items[i].Name == "Aged Brie")
+                        {
+                            // "Aged Brie" increases in Quality as it gets older
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                        else if (Items[i].Name == "Conjured Mana Cake")
+                        {
+                            // "Conjured" items degrade in Quality twice as fast as normal items
+                            // Decrease quality by 2 if sellIn is positive, and decrease by 4 if sellIn is negative
+                            if (Items[i].SellIn > 0)
+                            {
+                                Items[i].Quality = Items[i].Quality - 2;
+                            }
+                            else
+                            {
+                                Items[i].Quality = Items[i].Quality - 4;
+                            }
+                        }
+
+                        // Ensure quality does not exceed 50
+                        if (Items[i].Quality > 50)
+                        {
+                            Items[i].Quality = 50;
+                        }
+                    }
+                }
+
+                // SellIn value decreases for all items except "Sulfuras"
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                // Once the sell by date has passed, Quality degrades twice as fast
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].Name != "Conjured Mana Cake")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                // Decrease Quality by 1 for normal items after the sell by date
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+                        else
+                        {
+                            // "Conjured" items degrade in Quality twice as fast as normal items
+                            Items[i].Quality = Items[i].Quality - 2;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Name == "Aged Brie")
+                        {
+                            // "Aged Brie" increases in Quality even after the sell by date
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                        else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            // Quality drops to 0 after the concert
+                            Items[i].Quality = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GildedRoseTest.cs
@@ -0,0 +1,256 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        // Helper method to check if an item exists in the list of items
+        private bool ItemExists(IList<Item> items, string name)
+        {
+            foreach (var item in items)
+            {
+                if (item.Name == name)
+                    return true;
+            }
+            return false;
+        }
+
+        // Helper method to get an item by its name from the list of items
+        private Item GetItem(IList<Item> items, string name)
+        {
+            foreach (var item in items)
+            {
+                if (item.Name == name)
+                    return item;
+            }
+            return null;
+        }
+
+        [Test]
+        // Normal item quality should decrease by one after updating.
+        public void NormalItemQualityDecreasesByOne()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(19, GetItem(items, "+5 Dexterity Vest").Quality);
+        }
+
+        [Test]
+        // Normal item SellIn (days to sell) should decrease by one after updating.
+        public void NormalItemSellInDecreasesByOne()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(9, GetItem(items, "+5 Dexterity Vest").SellIn);
+        }
+
+        [Test]
+        // Normal item quality degrades twice as fast after SellIn (days to sell) becomes negative.
+        public void NormalItemQualityDegradesTwiceAsFastAfterSellIn()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(18, GetItem(items, "+5 Dexterity Vest").Quality);
+        }
+
+        [Test]
+        // Aged Brie quality increases by one after updating.
+        public void AgedBrieQualityIncreasesByOne()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(1, GetItem(items, "Aged Brie").Quality);
+        }
+
+        [Test]
+        // Aged Brie quality increases twice as fast after SellIn (days to sell) becomes negative.
+        public void AgedBrieQualityIncreasesTwiceAsFastAfterSellIn()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(2, GetItem(items, "Aged Brie").Quality);
+        }
+
+        [Test]
+        // Quality of an item should never exceed 50.
+        public void QualityNeverExceedsFifty()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(50, GetItem(items, "Aged Brie").Quality);
+        }
+
+        [Test]
+        // Sulfuras quality never changes after updating.
+        public void SulfurasQualityNeverChanges()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(80, GetItem(items, "Sulfuras, Hand of Ragnaros").Quality);
+        }
+
+        [Test]
+        // Backstage Pass quality increases by one after updating.
+        public void BackstagePassQualityIncreasesByOne()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(21, GetItem(items, "Backstage passes to a TAFKAL80ETC concert").Quality);
+        }
+
+        [Test]
+        // Backstage Pass quality increases by two when SellIn (days to sell) is ten or less.
+        public void BackstagePassQualityIncreasesByTwoWhenSellInIsTenOrLess()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(22, GetItem(items, "Backstage passes to a TAFKAL80ETC concert").Quality);
+        }
+
+        [Test]
+        // Backstage Pass quality increases by three when SellIn (days to sell) is five or less.
+        public void BackstagePassQualityIncreasesByThreeWhenSellInIsFiveOrLess()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(23, GetItem(items, "Backstage passes to a TAFKAL80ETC concert").Quality);
+        }
+
+        [Test]
+        // Backstage Pass quality drops to zero after the concert (SellIn becomes negative).
+        public void BackstagePassQualityDropsToZeroAfterConcert()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(0, GetItem(items, "Backstage passes to a TAFKAL80ETC concert").Quality);
+        }
+
+        [Test]
+        // Conjured item quality decreases by two after updating.
+        public void ConjuredItemQualityDecreasesByTwo()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(4, GetItem(items, "Conjured Mana Cake").Quality);
+        }
+
+        [Test]
+        // Conjured item quality degrades twice as fast after SellIn (days to sell) becomes negative.
+        public void ConjuredItemQualityDegradesTwiceAsFastAfterSellIn()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = -1, Quality = 6 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(2, GetItem(items, "Conjured Mana Cake").Quality);
+        }
+
+        [Test]
+        // Conjured item quality never drops below zero.
+        public void ConjuredItemQualityNeverDropsBelowZero()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 1 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.AreEqual(0, GetItem(items, "Conjured Mana Cake").Quality);
+        }
+
+        [Test]
+        // Non-existing item should be removed from the list after updating.
+        public void NonExistingItemDoesNotExistAfterUpdate()
+        {
+            // Arrange
+            IList<Item> items = new List<Item> { new Item { Name = "Non-existing Item", SellIn = 3, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.IsFalse(ItemExists(items, "Non-existing Item"));
+        }
+    }
+}
